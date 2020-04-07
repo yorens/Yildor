2e5f8e1b2b2f016b750ac1184ad542ae26a126f1
Steve Orens <steve@orens.com>
2020-04-07 11:16:55 -0700

    Add print lines to show when players take their turns

--- Splendor.py
+++ Splendor.py
@@ -2,9 +2,11 @@ from Splendor_Functions import *
 
 
 # Sample game
+print("Game Starting")
 game = create_2_player_game("Jessie", "Steven")
 start_game(game)
 draw_board(game)
+print("Player 1 takes turn 1")
 time.sleep(3)
 # player 1
 remove_chips_from_bank(game[1][4], "diamond", 1)
@@ -14,6 +16,7 @@ game[2][0][2][0] += 1
 game[2][0][2][2] += 1
 game[2][0][2][3] += 1
 draw_board(game)
+print("Player 2 takes turn 1")
 time.sleep(3)
 # player 2
 remove_chips_from_bank(game[1][4], "onyx", 2)
@@ -65,4 +68,3 @@ time.sleep(3)
 # player 2
 purchase_next_available_card(game, 2)
 draw_board(game)
-
