0fdf8c44b4378ca71c81da9e0a372de24f695e44
Yildiz Orens <y2014@orens.com>
2020-04-13 12:18:35 -0700

    Fixing time pauses between turns

--- Splendor.py
+++ Splendor.py
@@ -6,8 +6,8 @@ print("Game Starting")
 game = create_2_player_game("Jessie", "Steven")
 start_game(game)
 draw_board(game)
-print("Player 1 takes turn 1")
 time.sleep(3)
+print("Player 1 takes turn 1")
 # player 1
 remove_chips_from_bank(game[1][4], "diamond", 1)
 remove_chips_from_bank(game[1][4], "emerald", 1)
@@ -16,16 +16,16 @@ game[2][0][2][0] += 1
 game[2][0][2][2] += 1
 game[2][0][2][3] += 1
 draw_board(game)
-print("Player 2 takes turn 1")
 time.sleep(3)
+print("Player 2 takes turn 1")
 # player 2
 remove_chips_from_bank(game[1][4], "onyx", 2)
 game[2][1][2][0] += 1
 game[2][1][2][2] += 1
 game[2][1][2][3] += 1
 draw_board(game)
-print("Player 1 takes turn 2")
 time.sleep(3)
+print("Player 1 takes turn 2")
 # player 1
 remove_chips_from_bank(game[1][4], "sapphire", 1)
 remove_chips_from_bank(game[1][4], "onyx", 1)
@@ -34,8 +34,8 @@ game[2][0][2][1] += 1
 game[2][0][2][4] += 1
 game[2][0][2][3] += 1
 draw_board(game)
-print("Player 2 takes turn 2")
 time.sleep(3)
+print("Player 2 takes turn 2")
 # player 2
 remove_chips_from_bank(game[1][4], "sapphire", 1)
 remove_chips_from_bank(game[1][4], "onyx", 1)
@@ -44,8 +44,8 @@ game[2][1][2][1] += 1
 game[2][1][2][4] += 1
 game[2][1][2][3] += 1
 draw_board(game)
-print("Player 1 takes turn 3")
 time.sleep(3)
+print("Player 1 takes turn 3")
 # player 1
 remove_chips_from_bank(game[1][4], "diamond", 1)
 remove_chips_from_bank(game[1][4], "sapphire", 1)
@@ -54,8 +54,8 @@ game[2][0][2][0] += 1
 game[2][0][2][1] += 1
 game[2][0][2][4] += 1
 draw_board(game)
-print("Player 2 takes turn 3")
 time.sleep(3)
+print("Player 2 takes turn 3")
 # player 2
 remove_chips_from_bank(game[1][4], "diamond", 1)
 remove_chips_from_bank(game[1][4], "sapphire", 1)
@@ -64,13 +64,13 @@ game[2][1][2][0] += 1
 game[2][1][2][1] += 1
 game[2][1][2][4] += 1
 draw_board(game)
-print("Player 1 takes turn 4")
 time.sleep(3)
+print("Player 1 takes turn 4")
 # player 1
 purchase_next_available_card(game, 1)
 draw_board(game)
-print("Player 2 takes turn 4")
 time.sleep(3)
+print("Player 2 takes turn 4")
 # player 2
 purchase_next_available_card(game, 2)
 draw_board(game)
