b2e332d76313d01c1df303d8f8ce322ae035ad61
Yildiz Orens <y2014@orens.com>
2020-06-13 11:45:54 -0700

    Add player hand APIs
    * Add player unit tests for managing hand
    * Add card and chip value in player hand

--- player.py
+++ player.py
@@ -1,4 +1,65 @@
+import card
+
 class Player:
-    def __init__(self, number, name):
-        self.number = number
+    def __init__(self, name):
+        self.number = -1
         self.name = name
+        self.diamond_chips = 0
+        self.sapphire_chips = 0
+        self.emerald_chips = 0
+        self.ruby_chips = 0
+        self.onyx_chips = 0
+        self.diamond_cards = []
+        self.sapphire_cards = []
+        self.emerald_cards = []
+        self.ruby_cards = []
+        self.onyx_cards = []
+        self.wild_tokens = 0
+        
+    def emerald_card_buying(self):
+        return len(self.emerald_cards)
+    
+    def diamond_card_buying(self):
+        return len(self.diamond_cards)
+
+    def sapphire_card_buying(self):
+        return len(self.sapphire_cards)
+
+    def ruby_card_buying(self):
+        return len(self.ruby_cards)
+
+    def onyx_card_buying(self):
+        return len(self.onyx_cards)
+
+    def emerald_chip_buying(self):
+        return self.emerald_chips
+    
+    def diamond_chip_buying(self):
+        return self.diamond_chips
+
+    def sapphire_chip_buying(self):
+        return self.sapphire_chips
+
+    def ruby_chip_buying(self):
+        return self.ruby_chips
+
+    def onyx_chip_buying(self):
+        return self.onyx_chips
+
+    def add_card(self, card):
+        card_type = card.gem_type
+        if card_type == "diamond":
+            self.diamond_cards.append(card)
+
+        elif card_type == "sapphire":
+            self.sapphire_cards.append(card)
+
+        elif card_type == "emerald":
+            self.emerald_cards.append(card)
+        
+        elif card_type == "ruby":
+            self.ruby_cards.append(card)
+
+        elif card_type == "onyx":
+            self.onyx_cards.append(card)
+
--- tests/player_tests.py
+++ tests/player_tests.py
@@ -1,10 +1,119 @@
 import unittest
+import player
+import card
 
 
 class TestPlayerMethods(unittest.TestCase):
 
-    def test_replaceme(self):
-        self.assertTrue(True)
+    def test_one_diamond_card(self):
+        player_1 = player.Player("Yildiz")
+        diamond = card.Card(1, 0, "diamond", 2, 2, 1, 0, 0)
+        player_1.add_card(diamond)
+        self.assertTrue(player_1.diamond_card_buying() == 1)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 0)
+        self.assertTrue(player_1.ruby_card_buying() == 0)
+        self.assertTrue(player_1.onyx_card_buying() == 0)
+    
+    def test_one_sapphire_card(self):
+        player_1 = player.Player("Yildiz")
+        sapphire = card.Card(1, 0, "sapphire", 2, 2, 1, 0, 0)
+        player_1.add_card(sapphire)
+        self.assertTrue(player_1.diamond_card_buying() == 0)
+        self.assertTrue(player_1.sapphire_card_buying() == 1)
+        self.assertTrue(player_1.emerald_card_buying() == 0)
+        self.assertTrue(player_1.ruby_card_buying() == 0)
+        self.assertTrue(player_1.onyx_card_buying() == 0)
+    
+    def test_one_emerald_card(self):
+        player_1 = player.Player("Yildiz")
+        emerald = card.Card(1, 0, "emerald", 2, 2, 1, 0, 0)
+        player_1.add_card(emerald)
+        self.assertTrue(player_1.diamond_card_buying() == 0)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 1)
+        self.assertTrue(player_1.ruby_card_buying() == 0)
+        self.assertTrue(player_1.onyx_card_buying() == 0)
+    
+    def test_one_ruby_card(self):
+        player_1 = player.Player("Yildiz")
+        ruby = card.Card(1, 0, "ruby", 2, 2, 1, 0, 0)
+        player_1.add_card(ruby)
+        self.assertTrue(player_1.diamond_card_buying() == 0)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 0)
+        self.assertTrue(player_1.ruby_card_buying() == 1)
+        self.assertTrue(player_1.onyx_card_buying() == 0)
+    
+    def test_one_onyx_card(self):
+        player_1 = player.Player("Yildiz")
+        onyx = card.Card(1, 0, "onyx", 2, 2, 1, 0, 0)
+        player_1.add_card(onyx)
+        self.assertTrue(player_1.diamond_card_buying() == 0)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 0)
+        self.assertTrue(player_1.ruby_card_buying() == 0)
+        self.assertTrue(player_1.onyx_card_buying() == 1)
+
+    def test_two_cards(self):
+        player_1 = player.Player("Yildiz")
+        player_1.add_card(card.Card(1, 0, "onyx", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "diamond", 2, 2, 1, 0, 0))
+        self.assertTrue(player_1.diamond_card_buying() == 1)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 0)
+        self.assertTrue(player_1.ruby_card_buying() == 0)
+        self.assertTrue(player_1.onyx_card_buying() == 1)
+
+    def test_three_cards(self):
+        player_1 = player.Player("Yildiz")
+        player_1.add_card(card.Card(1, 0, "onyx", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "diamond", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "ruby", 2, 2, 1, 0, 0))
+        self.assertTrue(player_1.diamond_card_buying() == 1)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 0)
+        self.assertTrue(player_1.ruby_card_buying() == 1)
+        self.assertTrue(player_1.onyx_card_buying() == 1)
+
+    def test_four_cards(self):
+        player_1 = player.Player("Yildiz")
+        player_1.add_card(card.Card(1, 0, "onyx", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "diamond", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "ruby", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "emerald", 2, 2, 1, 0, 0))
+        self.assertTrue(player_1.diamond_card_buying() == 1)
+        self.assertTrue(player_1.sapphire_card_buying() == 0)
+        self.assertTrue(player_1.emerald_card_buying() == 1)
+        self.assertTrue(player_1.ruby_card_buying() == 1)
+        self.assertTrue(player_1.onyx_card_buying() == 1)
+
+    def test_five_cards(self):
+        player_1 = player.Player("Yildiz")
+        player_1.add_card(card.Card(1, 0, "onyx", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "diamond", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "ruby", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "emerald", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "sapphire", 2, 2, 1, 0, 0))
+        self.assertTrue(player_1.diamond_card_buying() == 1)
+        self.assertTrue(player_1.sapphire_card_buying() == 1)
+        self.assertTrue(player_1.emerald_card_buying() == 1)
+        self.assertTrue(player_1.ruby_card_buying() == 1)
+        self.assertTrue(player_1.onyx_card_buying() == 1)
+
+    def test_two_diamond_cards(self):
+        player_1 = player.Player("Yildiz")
+        player_1.add_card(card.Card(1, 0, "onyx", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "diamond", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "diamond", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "ruby", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "emerald", 2, 2, 1, 0, 0))
+        player_1.add_card(card.Card(1, 0, "sapphire", 2, 2, 1, 0, 0))
+        self.assertTrue(player_1.diamond_card_buying() == 2)
+        self.assertTrue(player_1.sapphire_card_buying() == 1)
+        self.assertTrue(player_1.emerald_card_buying() == 1)
+        self.assertTrue(player_1.ruby_card_buying() == 1)
+        self.assertTrue(player_1.onyx_card_buying() == 1)
 
 
 if __name__ == '__main__':
