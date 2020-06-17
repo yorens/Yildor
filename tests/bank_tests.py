48a84c99c37d42fd3b32e7c508dd7f722ea4c047
Yildiz Orens <y2014@orens.com>
2020-06-16 21:40:17 -0700

    Changing string names to factory types in bank

--- tests/bank_tests.py
+++ tests/bank_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import bank
+import yildor
 
 
 class TestBankMethods(unittest.TestCase):
@@ -9,31 +10,31 @@ class TestBankMethods(unittest.TestCase):
 
     def test_take_away_one_chip_from_bank(self):
         b = bank.Bank(7, 7, 8, 7, 7)
-        b.withdraw_one_coin("emerald")
-        self.assertTrue(b.balance("emerald") == 7)
+        b.withdraw_one_coin(yildor.emerald)
+        self.assertTrue(b.balance(yildor.emerald) == 7)
 
     def test_withdraw_too_much(self):
         b = bank.Bank(7, 7, 8, 7, 7)
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        b.withdraw_one_coin("emerald")
-        self.assertTrue(b.balance("emerald") == 0)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        b.withdraw_one_coin(yildor.emerald)
+        self.assertTrue(b.balance(yildor.emerald) == 0)
 
     def test_deposit_too_many_coins(self):
         b = bank.Bank(7, 7, 7, 7, 7)
-        b.add_chip_to_bank("diamond")
-        b.add_chip_to_bank("diamond")
-        self.assertTrue(b.balance("diamond") == 7)
+        b.add_chip_to_bank(yildor.diamond)
+        b.add_chip_to_bank(yildor.diamond)
+        self.assertTrue(b.balance(yildor.diamond) == 7)
 
 if __name__ == '__main__':
     unittest.main()
