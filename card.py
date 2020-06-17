5d947ad3b6e06960f4447854acf63712220cf2c2
Steve Orens <steve@orens.com>
2020-06-16 22:48:51 -0700

    Update Card class to set and return individual costs
    * Add `set_cost_*(self, value)` with chaining (return self)
    * Add `cost_*()` to return cost value
    * Add unit tests to validate injected data

--- card.py
+++ card.py
@@ -1,7 +1,45 @@
 class Card:
-    def __init__(self, rank, point_value, gem_type, diamond_cost, sapphire_cost, emerald_cost, ruby_cost, onyx_cost):
+    def __init__(self, rank, point_value, gem_type, diamond_cost=0, sapphire_cost=0, emerald_cost=0, ruby_cost=0, onyx_cost=0):
         self.rank = rank
         self.point_value = point_value
         self.gem_type = gem_type
-        cost_string = str(diamond_cost) + str(sapphire_cost) + str(emerald_cost) + str(ruby_cost) + str(onyx_cost)
-        self.cost_string = cost_string
+        self.diamond_cost = diamond_cost
+        self.sapphire_cost = sapphire_cost
+        self.emerald_cost = emerald_cost
+        self.ruby_cost = ruby_cost
+        self.onyx_cost = onyx_cost
+
+    def set_cost_diamond(self, value):
+        self.diamond_cost = value
+        return self
+
+    def set_cost_sapphire(self, value):
+        self.sapphire_cost = value
+        return self
+
+    def set_cost_emerald(self, value):
+        self.emerald_cost = value
+        return self
+
+    def set_cost_ruby(self, value):
+        self.ruby_cost = value
+        return self
+
+    def set_cost_onyx(self, value):
+        self.onyx_cost = value
+        return self
+
+    def cost_diamond(self):
+        return self.diamond_cost
+
+    def cost_sapphire(self):
+        return self.sapphire_cost
+
+    def cost_emerald(self):
+        return self.emerald_cost
+
+    def cost_ruby(self):
+        return self.ruby_cost
+
+    def cost_onyx(self):
+        return self.onyx_cost
--- tests/card_tests.py
+++ tests/card_tests.py
@@ -1,11 +1,70 @@
 import unittest
-
+import card
+import yildor
 
 class TestCardMethods(unittest.TestCase):
 
-    def test_replaceme(self):
-        self.assertTrue(True)
+    def test_card_set_cost_diamond(self):
+        card_1 = card.Card(1, 0, yildor.sapphire)
+        card_1.set_cost_diamond(2)
+        self.assertTrue(card_1.cost_diamond() == 2)
 
+    def test_card_set_cost_diamond_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire, 2)
+        self.assertTrue(card_1.cost_diamond() == 2)
+
+    def test_card_set_cost_sapphire(self):
+        card_1 = card.Card(1, 0, yildor.sapphire)
+        card_1.set_cost_sapphire(2)
+        self.assertTrue(card_1.cost_sapphire() == 2)
+
+    def test_card_set_cost_sapphire_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire, 0, 2)
+        self.assertTrue(card_1.cost_sapphire() == 2)
+
+    def test_card_set_cost_emerald(self):
+        card_1 = card.Card(1, 0, yildor.sapphire)
+        card_1.set_cost_emerald(2)
+        self.assertTrue(card_1.cost_emerald() == 2)
+
+    def test_card_set_cost_emerald_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire, 0, 0, 2)
+        self.assertTrue(card_1.cost_emerald() == 2)
+
+    def test_card_set_cost_ruby(self):
+        card_1 = card.Card(1, 0, yildor.sapphire)
+        card_1.set_cost_ruby(2)
+        self.assertTrue(card_1.cost_ruby() == 2)
+
+    def test_card_set_cost_ruby_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire, 0, 0, 0, 2)
+        self.assertTrue(card_1.cost_ruby() == 2)
+
+    def test_card_set_cost_onyx(self):
+        card_1 = card.Card(1, 0, yildor.sapphire)
+        card_1.set_cost_onyx(2)
+        self.assertTrue(card_1.cost_onyx() == 2)
+
+    def test_card_set_cost_onyx_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire, 0, 0, 0, 0, 2)
+        self.assertTrue(card_1.cost_onyx() == 2)
+
+    def test_card_set_multiple_costs_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire, 1, 2, 3, 4, 5)
+        self.assertTrue(card_1.cost_diamond() == 1)
+        self.assertTrue(card_1.cost_sapphire() == 2)
+        self.assertTrue(card_1.cost_emerald() == 3)
+        self.assertTrue(card_1.cost_ruby() == 4)
+        self.assertTrue(card_1.cost_onyx() == 5)
+
+    def test_card_set_multiple_costs_in_constructor(self):
+        card_1 = card.Card(1, 0, yildor.sapphire)
+        card_1.set_cost_diamond(1).set_cost_sapphire(2).set_cost_emerald(3).set_cost_ruby(4).set_cost_onyx(5)
+        self.assertTrue(card_1.cost_diamond() == 1)
+        self.assertTrue(card_1.cost_sapphire() == 2)
+        self.assertTrue(card_1.cost_emerald() == 3)
+        self.assertTrue(card_1.cost_ruby() == 4)
+        self.assertTrue(card_1.cost_onyx() == 5)
 
 if __name__ == '__main__':
     unittest.main()
