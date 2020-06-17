bd3b1e670c9c4d1355c97460b7d23601e4c7fd90
Steve Orens <steve@orens.com>
2020-06-16 21:59:25 -0700

    Remove _from_deck, improve unit test

--- deck.py
+++ deck.py
@@ -5,7 +5,7 @@ class Deck:
     def add_card(self, card):
         self.deck.append(card)
 
-    def remove_card_from_deck(self, input_card):
+    def remove_card(self, input_card):
         for card in self.deck:
             if card == input_card:
                 self.deck.remove(input_card)
--- tests/deck_tests.py
+++ tests/deck_tests.py
@@ -5,15 +5,17 @@ import yildor
 
 class TestDeckMethods(unittest.TestCase):
 
-    def test_remove_card_from_deck(self):
+    def test_deck_remove_card(self):
         deck_1 = deck.Deck()
         card_1 = card.Card(1, 0, yildor.sapphire, 2, 2, 1, 0, 0)
-        deck_1.add_card(card_1)
         card_2 = card.Card(1, 0, yildor.emerald, 2, 0, 1, 0, 2)
+        card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
+        deck_1.add_card(card_1)
         deck_1.add_card(card_2)
-        # card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
-        deck_1.remove_card_from_deck(card_2)
+        deck_1.remove_card(card_2)
         self.assertTrue(deck_1.contains(card_1))
+        self.assertFalse(deck_1.contains(card_2))
+        self.assertFalse(deck_1.contains(card_3))
 
 
 if __name__ == '__main__':
