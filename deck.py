17a2f4c5635ccb3fe747ebfa505b0d4f27b1f73b
Yildiz Orens <y2014@orens.com>
2020-06-16 21:15:13 -0700

    Create deck tests for removing a card

--- deck.py
+++ deck.py
@@ -1,3 +1,18 @@
 class Deck:
     def __init__(self):
         self.deck = []
+
+    def add_card(self, card):
+        self.deck.append(card)
+
+    def remove_card_from_deck(self, input_card):
+        for card in self.deck:
+            if card == input_card:
+                self.deck.remove(input_card)
+
+    def contains(self, input_card):
+        for card in self.deck:
+            if card == input_card:
+                return True
+        return False
+                
--- tests/deck_tests.py
+++ tests/deck_tests.py
@@ -1,10 +1,19 @@
+import card
+import deck
 import unittest
-
+import yildor
 
 class TestDeckMethods(unittest.TestCase):
 
-    def test_replaceme(self):
-        self.assertTrue(True)
+    def test_remove_card_from_deck(self):
+        deck_1 = deck.Deck()
+        card_1 = card.Card(1, 0, yildor.sapphire, 2, 2, 1, 0, 0)
+        deck_1.add_card(card_1)
+        card_2 = card.Card(1, 0, yildor.emerald, 2, 0, 1, 0, 2)
+        deck_1.add_card(card_2)
+        # card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
+        deck_1.remove_card_from_deck(card_2)
+        self.assertTrue(deck_1.contains(card_1))
 
 
 if __name__ == '__main__':
