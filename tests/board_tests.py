29aa662279fda9b5ed574a02af968ba20291b553
Steve Orens <steve@orens.com>
2020-06-17 05:50:38 -0700

    Cleanup board_tests
    * Forgot to remove unused contextmanager
    * Improve method names
    * Add additional comments

--- tests/board_tests.py
+++ tests/board_tests.py
@@ -1,5 +1,4 @@
 import board
-from contextlib import contextmanager
 import io
 import sys
 import unittest
@@ -7,42 +6,48 @@ import unittest
 
 class TestBoardMethods(unittest.TestCase):
 
-    @contextmanager
+    # before each test, capture the sys.stdout and sys.stderr
     def setUp(self):
-        self.test_output = io.StringIO()
+        self.test_out = io.StringIO()
         self.test_err = io.StringIO()
         self.original_output = sys.stdout
         self.original_err = sys.stderr
-        sys.stdout = self.test_output
+        sys.stdout = self.test_out
         sys.stderr = self.test_err
 
+    # restore sys.stdout and sys.stderr after each test
     def tearDown(self):
         sys.stdout = self.original_output
         sys.stderr = self.original_err
 
-    def assertPrintedEquals(self, value):
-        if self.test_output == None:
-            return False
+    # assert that sys.stdout would be equal to expected value
+    def assertStdoutEquals(self, value):
+        self.assertEqual(self.test_out.getvalue().strip(), value)
 
-        return self.test_output.getvalue().strip() == value
+    # assert that sys.stdout would not be equal to expected value
+    def assertStdoutNotEquals(self, value):
+        self.assertNotEqual(self.test_out.getvalue().strip(), value)
 
-    def assertPrintedNotEquals(self, value):
-        if self.test_output == None:
-            return False
+    # assert that sys.stderr would be equal to expected value
+    def assertStderrEquals(self, value):
+        self.assertEqual(self.test_err.getvalue().strip(), value)
 
-        return self.test_output.getvalue().strip() != value
+    # assert that sys.stderr would not be equal to expected value
+    def assertStderrNotEquals(self, value):
+        self.assertNotEqual(self.test_err.getvalue().strip(), value)
 
-    def test_board_replaceme(self):
-        self.assertTrue(True)
+    #
+    # unit tests go here
+    #
 
     # example of unit test that can capture the printed output
-    def test_board_replaceme2(self):
+    def test_board_replaceme(self):
         b = board.Board()
         b.print_top_line()
 
-        # use assertPrintedEquals(value) to test if your
+        # use assertStdoutEquals(value) to test if your
         # printed value matches your expected `value`
-        self.assertPrintedEquals("------")
+        self.assertStdoutEquals("------")
 
 
 if __name__ == '__main__':
