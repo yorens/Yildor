b7d20c8cc81ffd66fc60f3f248b66a54fd89284b
Steve Orens <steve@orens.com>
2020-06-16 23:42:42 -0700

    Add additional deck unit tests (not working)
    These new tests are missing implementation details and will not pass until those methods are implemented.

--- tests/deck_tests.py
+++ tests/deck_tests.py
@@ -12,10 +12,11 @@ class TestDeckMethods(unittest.TestCase):
         card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
         deck_1.add_card(card_1)
         deck_1.add_card(card_2)
+        deck_1.add_card(card_3)
         deck_1.remove_card(card_2)
         self.assertTrue(deck_1.contains(card_1))
         self.assertFalse(deck_1.contains(card_2))
-        self.assertFalse(deck_1.contains(card_3))
+        self.assertTrue(deck_1.contains(card_3))
 
     def test_deck_deal_one_card(self):
         deck_1 = deck.Deck()
@@ -33,7 +34,53 @@ class TestDeckMethods(unittest.TestCase):
         self.assertTrue(deck_1.deal_card() == None)
         self.assertTrue(deck_1.deal_card() == None)
 
+    def test_length_of_deck(self):
+        deck_1 = deck.Deck()
+        card_1 = card.Card(1, 0, yildor.sapphire, 2, 2, 1, 0, 0)
+        card_2 = card.Card(1, 0, yildor.emerald, 2, 0, 1, 0, 2)
+        card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
+        deck_1.add_card(card_1)
+        deck_1.add_card(card_2)
+        deck_1.add_card(card_3)
+        self.assertTrue(deck_1.length() == 3)
 
+    def test_length_of_deck_after_dealing_card(self):
+        deck_1 = deck.Deck()
+        card_1 = card.Card(1, 0, yildor.sapphire, 2, 2, 1, 0, 0)
+        card_2 = card.Card(1, 0, yildor.emerald, 2, 0, 1, 0, 2)
+        card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
+        deck_1.add_card(card_1)
+        deck_1.add_card(card_2)
+        deck_1.add_card(card_3)
+        check = deck_1.deal_card()
+        self.assertTrue(deck_1.length() == 2)
+        self.assertTrue(check == card_1)
+
+    def test_deal_cards_until_empty_deck_returns_length_zero(self):
+        deck_1 = deck.Deck()
+        card_1 = card.Card(1, 0, yildor.sapphire, 2, 2, 1, 0, 0)
+        card_2 = card.Card(1, 0, yildor.emerald, 2, 0, 1, 0, 2)
+        card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
+        deck_1.add_card(card_1)
+        deck_1.add_card(card_2)
+        deck_1.add_card(card_3)
+        check = deck_1.deal_card()
+        while deck_1.length() > 0:
+            check = deck_1.deal_card()
+        self.assertTrue(deck_1.length() == 0)
+
+    def test_deal_cards_until_empty_deck_returns_none_card(self):
+        deck_1 = deck.Deck()
+        card_1 = card.Card(1, 0, yildor.sapphire, 2, 2, 1, 0, 0)
+        card_2 = card.Card(1, 0, yildor.emerald, 2, 0, 1, 0, 2)
+        card_3 = card.Card(1, 0, yildor.diamond, 2, 0, 1, 0, 2)
+        deck_1.add_card(card_1)
+        deck_1.add_card(card_2)
+        deck_1.add_card(card_3)
+        check = deck_1.deal_card()
+        while check != None:
+            check = deck_1.deal_card()
+        self.assertTrue(check == None)
 
 if __name__ == '__main__':
     unittest.main()
